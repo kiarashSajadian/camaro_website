e7b0a5fa09e220720027ea22d97334c3fe276a9e
Kiarash Sajadian <kiarashsajadian@gmail.com>
2025-02-22 12:05:26 +0100

    Create smooth_scroll.js

---
+++ smooth_scroll.js
@@ -0,0 +1,17 @@
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  direction: "vertical",
+  smooth: true,
+  mouseMultiplier: 1,
+  smoothTouch: false,
+  touchMultiplier: 2,
+  infinite: false,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
